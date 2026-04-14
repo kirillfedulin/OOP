ccf2547bba79f3727ca1d683ac2dd36f5283d63c
opilane <opilane@HP-CZC2349HV2>
2026-04-14 09:53:45 +0300

    Add project files.

---
+++ ConsoleApp1/Isik.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    //public class Isik
+    //{
+        //Omadused ehk väljad
+        //public string Nimi;
+        //public int Vanus;   
+
+         //Tegevus ehk meetod
+        //public void Tervita()
+        //{
+        //    Console.WriteLine($"Tere! Mina olen {Nimi} ja ma olen {Vanus} aastat vana.");
+        //}
+    //}
+
+    public class Isik
+    {
+        // Privaatne väli - otse ligi ei saa
+        private int sünniaasta;
+
+        // Avalik omadus (Property) automaatse get/set logikaga
+        public string Nimi { get; set; }
+
+        public void Ask()
+        {
+            Console.WriteLine("sissesta sinu sünniaasta: ");
+            int answer = Convert.ToInt32(Console.ReadLine());
+        }
+        // Kontrollitud omadus
+        public int Sünniaasta
+        {
+            get { return sünniaasta; }
+            set
+            {
+                if (value > 1900 && value <= DateTime.Now.Year)
+                    sünniaasta = value;
+                else
+                    Console.WriteLine("Vigane sünniaasta!");
+            }
+        }
+        public void Tervita()
+        {
+            if (string.IsNullOrEmpty(Nimi) || sünniaasta == 0)
+            {
+                Console.WriteLine("See on viga aasta");
+            }
+            else
+            {
+                Console.WriteLine($"{Vanus} aastat vana.");
+            }
+        }
+
+
+        // Arvutatud omadus (ainult lugemiseks / getter)
+        public int Vanus => DateTime.Now.Year - sünniaasta;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Isik inimene1 = new Isik();
+            //inimene1.Nimi = "Mati";
+            //inimene1.Vanus = 35;
+            //inimene1.Tervita(); // Väljund: Tere! Mina olen Mati...
+
+            //Isik2 inimene1 = new Isik2();
+            //inimene1.Nimi = "Mati";
+            //inimene1.Sünniaasta = 2009;
+            ///inimene1.Tervita();
+            // Väljund: Tere! Mina olen Mati...
+
+            Õpetaja õpetaja1 = new Õpetaja();
+            õpetaja1.Sünniaasta = 1875;
+            õpetaja1.Aine = "Programitimine";
+            õpetaja1.Õpeta();
+            Õpilane õpilane1 = new Õpilane();
+            õpilane1.Sünniaasta = 2009;
+            õpilane1.Kool = "TheSchool";
+            õpilane1.Klass = 9;
+            õpilane1.Õpi();
+
+
+        }
+    }
+}
